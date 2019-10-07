6fc7bd501510f80b94b4c73e58a3c318a72861c2
李文超 <lwc@meitu.com>
2019-10-07 21:57:21 +0800

    EditDistance.java

---
+++ lintcode/app/src/main/java/com/bytetree/lintcode/dp/EditDistance.java
@@ -0,0 +1,72 @@
+package com.bytetree.lintcode.dp;
+
+/**
+ * 119. Edit Distance
+ * <p>
+ * Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+ * <p>
+ * You have the following 3 operations permitted on a word:
+ * <p>
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ * <p>
+ * Example
+ * Example 1:
+ * <p>
+ * Input:
+ * "horse"
+ * "ros"
+ * Output: 3
+ * Explanation:
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ * Example 2:
+ * <p>
+ * Input:
+ * "intention"
+ * "execution"
+ * Output: 5
+ * Explanation:
+ * intention -> inention (remove 't')
+ * inention -> enention (replace 'i' with 'e')
+ * enention -> exention (replace 'n' with 'x')
+ * exention -> exection (replace 'n' with 'c')
+ * exection -> execution (insert 'u')
+ * <p>
+ * <a href="https://www.lintcode.com/problem/edit-distance/description">119. Edit Distance</a>
+ * <p>
+ * Created by vencial on 2019-10-07.
+ */
+public class EditDistance {
+
+    /**
+     * @param word1: A string
+     * @param word2: A string
+     * @return: The minimum number of steps.
+     */
+    public int minDistance(String word1, String word2) {
+        // write your code here
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
